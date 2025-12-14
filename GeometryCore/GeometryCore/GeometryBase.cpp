342ff87f40d647e31baff313a197a2bd77a89bf4
DasAneesh <149708161+DasAneesh@users.noreply.github.com>
2025-12-14 13:00:18 +0300

    Only GeometryBase Completed

---
+++ GeometryCore/GeometryCore/GeometryBase.cpp
@@ -0,0 +1,105 @@
+#include "GeometryBase.h"
+
+
+Vector::Vector(fType dx, fType dy) : dx(dx), dy(dy) {}
+
+Vector Vector::operator+(const Vector& other) const {
+	return Vector(this->dx + other.dx, this->dy + other.dy);
+}
+
+Vector Vector::operator-(const Vector& other) const {
+	return Vector(this->dx - other.dx, this->dy - other.dy);
+}
+
+Vector Vector::operator*(fType scalar) const {
+	return Vector(this->dx * scalar, this->dy * scalar);
+}
+
+Vector Vector::operator/(fType scalar) const {
+	if(std::abs(scalar) < Elipson)
+		return Vector(this->dx / scalar, this->dy / scalar);
+	throw std::invalid_argument("Vector cannot divide by 0.");
+}
+
+bool Vector::operator==(const Vector& other) const {
+	return Elipson > std::abs(this->dx - other.dx) && Elipson > std::abs(this->dy - other.dy);
+}
+bool Vector::operator!=(const Vector& other) const {
+	return Elipson < std::abs(this->dx - other.dx) || Elipson < std::abs(this->dy - other.dy);
+
+	// return !(*this == other)
+}
+fType Vector::length() const {
+	return std::sqrt(this->dx * this->dx + this->dy * this->dy);
+}
+
+Vector Vector::normalized() const {
+	fType len = length();
+	if (std::abs(len) < Elipson) { return Vector(0, 0); }
+	return Vector(this->dx / len, this->dy / len);
+
+}
+
+fType Vector::dotProduct(const Vector& other) const {
+	return this->dx * other.dx + this->dy * other.dy;
+}
+
+fType Vector::crossProduct(const Vector& other) const {
+	return this->dx * other.dy - this->dy * other.dx;
+}
+
+fType Vector::getX() const {
+	return this->dx;
+}
+
+fType Vector::getY() const {
+	return this->dy;
+}
+
+Point::Point(fType x, fType y) : x(x), y(y) {}
+
+Point::Point(const Vector& v) : x(v.dx), y(v.dy) {}
+
+Point Point::operator+(const Vector& v) const {
+	return Point(x + v.dx, y + v.dy);
+}
+
+Point Point::operator-(const Vector& v) const {
+	return Point(x - v.dx, y - v.dy);
+}
+
+Vector Point::operator-(const Point& other) const {
+	return Vector(x - other.x, y - other.y);
+}
+
+bool Point::operator==(const Point& other) const {
+	return std::abs(x - other.x) < Elipson && std::abs(y - other.y) < Elipson;
+}
+
+bool Point::operator!=(const Point& other) const {
+	return !(*this == other);
+}
+
+fType Point::distanceTo(const Point& other) const {
+	return (*this - other).length();
+}
+
+Point Point::rotate(const Point& center, fType angle) const {
+	Vector v = *this - center;
+
+	fType cosA = std::cos(angle);
+	fType sinA = std::sin(angle);
+
+	fType newX = v.dx * cosA - v.dy * sinA;
+	fType newY = v.dx * sinA + v.dy * cosA;
+
+	return center + Vector(newX, newY);
+}
+
+
+Point Point::translate(const Vector& v) const {
+	return *this + v;
+}
+
+fType Point::getX() const { return x; }
+fType Point::getY() const { return y; }
---
+++ GeometryCore/GeometryCore/GeometryBase.h
@@ -0,0 +1,57 @@
+#pragma once
+#include <cmath>
+#include <iostream>
+
+using fType = double;
+const fType Elipson= 1e-6;
+
+class Point;
+
+class Vector {
+public:
+
+	fType dx, dy;
+
+	Vector(fType dx = 0, fType dy = 0) {}
+
+	//operators
+	Vector operator+(const Vector& other) const;
+	Vector operator-(const Vector& other) const;
+	Vector operator*(fType scalar) const;
+	Vector operator/(fType scalar) const;
+	bool operator==(const Vector& other) const;
+	bool operator!=(const Vector& other) const;
+
+	//methods
+	fType length() const;
+	Vector normalized() const;
+	fType dotProduct(const Vector& other) const;
+	fType crossProduct(const Vector& other) const;
+
+	fType getX() const;
+	fType getY() const;
+
+};
+
+class Point {
+public:
+	fType x, y;
+
+	Point(fType x = 0, fType y = 0) {}
+	Point(const Vector& v);
+
+	//operators
+	Point operator+(const Vector& v) const;
+	Point operator-(const Vector& v) const;
+	Vector operator-(const Point& other) const;
+	bool operator==(const Point& other) const;
+	bool operator!=(const Point& other) const;
+
+	//methods
+	fType distanceTo(const Point& other) const;
+	Point rotate(const Point& center, fType angle) const;
+	Point translate(const Vector& v) const;
+
+	fType getX() const;
+	fType getY() const;
+};
